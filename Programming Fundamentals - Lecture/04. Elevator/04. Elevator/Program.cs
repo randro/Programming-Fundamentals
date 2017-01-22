36e0dc7273cd75983dc917dd5dd367bd235a32b3
randro <rbakiev94@gmail.com>
2017-01-22 18:07:27 +0200

    Lecture

---
+++ Programming Fundamentals - Lecture/04. Elevator/04. Elevator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = int.Parse(Console.ReadLine());
+            var capacity = int.Parse(Console.ReadLine());
+            var courses = (int)Math.Ceiling((double)person / capacity);
+
+            Console.WriteLine(courses);
+
+
+        }
+    }
+}
---
+++ Programming Fundamentals - Lecture/07. Greeting/07. Greeting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string ageStr = Console.ReadLine();
+            int age = int.Parse(ageStr); // Parse string  int
+            Console.WriteLine($"Hello, {firstName} {lastName}.\r\nYou are {age} years old.");
+
+
+
+
+        }
+    }
+}
