c61389bbc831e624911149e39dc0c314918ba35f
randro <rbakiev94@gmail.com>
2017-01-25 13:42:59 +0200

    New update

---
+++ 3.Methods. Debugging and Troubleshooting Code/5. Calculate Triangle Area/5. Calculate Triangle Area/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Calculate_Triangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            Console.WriteLine(GetTriangleArea(width, height));
+
+        }
+        static double GetTriangleArea(double width, double height)
+        {
+            return width * height / 2;
+        }
+
+    }
+}
