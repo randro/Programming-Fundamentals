91083ab3056566a3772274a89e760166c935e574
randro <rbakiev94@gmail.com>
2017-01-29 11:36:04 +0200

    Arrays new

---
+++ 5.Arrays - Lab/2. Reverse an Array of Integers/2. Reverse an Array of Integers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace _2.Reverse_an_Array_of_Integers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var numbers = new int[n];
+
+            for (int i = 0; i <numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = numbers.Length-1; i >=0; i--)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
---
+++ 5.Arrays - Lab/3. Last K Numbers Sums Sequence/3. Last K Numbers Sums Sequence/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Last_K_Numbers_Sums_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var k = int.Parse(Console.ReadLine());
+
+            var firstArr = new int[0];
+            firstArr[0] = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                
+            }
+
+               
+        }
+    }
+}
