4565015b3d1da4b7d73fb0de790ed5886c2c4865
randro <rbakiev94@gmail.com>
2017-01-31 19:49:25 +0200

    Lists

---
+++ 7.Lists-Lab/1. Remove Negatives and Reverse/1. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _1.Remove_Negatives_and_Reverse
+{
+    public class RemovindandReversing
+    {
+        public static void Main(string[] args)
+        {
+            string values = Console.ReadLine();
+
+            List<string> items = values.Split(' ').ToList();
+
+            var nums = new List<int>();
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                nums.Add(int.Parse(items[i]));
+            }
+
+
+            nums.Reverse();
+            nums.RemoveAll(x => x <= 0);
+
+                foreach (var number in nums)
+
+                {
+                    Console.WriteLine(number);
+                }
+            
+        }
+    }
+}
---
+++ 7.Lists-Lab/3. Sum Adjacent Equal Numbers/3. Sum Adjacent Equal Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _3.Sum_Adjacent_Equal_Numbers
+{
+   public class Sum
+    {
+       public  static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            List<string> items = number.Split(' ').ToList();
+
+            List<int> nums = new List<int>();
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                nums.Add(int.Parse(items[i]));
+            }
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+
+            }
+        }
+    }
+}
