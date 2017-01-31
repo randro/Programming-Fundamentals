d878b809f17ed8265996c4cc7ae638bc6853311a
randro <rbakiev94@gmail.com>
2017-01-31 19:49:50 +0200

    Last update

---
+++ 7.Lists-Lab/2. Append Lists/2. Append Lists/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _2.Append_Lists
+{
+    public class Append
+    {
+        public static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+
+            List<string> items = n.Split('|', ' ').ToList();
+            items.Remove();
+
+            var nums = new List<int>();
+            
+
+            foreach (var item in items)
+
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+
+    }
+}
+
